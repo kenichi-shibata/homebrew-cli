134daacf91c26b650f545618b1f2f5fc48ac555b
Marius Karnauskas <cofkie@gmail.com>
2015-05-06 11:48:21 +0100

    ccat

---
+++ ccat.rb
@@ -0,0 +1,14 @@
+class Ccat < Formula
+    version '0.0.1'
+    homepage "https://github.com/jingweno/ccat"
+    url "https://github.com/jingweno/ccat/archive/v#{version}.tar.gz"
+    sha256 "8847c8908779a3502f8621aa8ad34d92a2655b1f18008beeb291daaea785592f"
+
+    depends_on "go" => :build
+
+    def install
+        ENV["GOPATH"] = buildpath
+        system "go build -o ccat"
+        bin.install "ccat"
+    end
+end
