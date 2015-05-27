c924b8d0234d6f1db9fbbae29f2f37ba137d5dca
Marius Karnauskas <cofkie@gmail.com>
2015-05-27 02:04:50 +0100

    fix

--- ccat.rb
+++ ccat.rb
@@ -1,7 +1,6 @@
 class Ccat < Formula
-    version '0.0.1'
     homepage "https://github.com/jingweno/ccat"
-    url "https://github.com/jingweno/ccat/archive/v#{version}.tar.gz"
+    url "https://github.com/jingweno/ccat/archive/v0.0.1.tar.gz"
     sha256 "8847c8908779a3502f8621aa8ad34d92a2655b1f18008beeb291daaea785592f"
 
     depends_on "go" => :build
