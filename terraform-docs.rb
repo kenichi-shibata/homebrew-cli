327a8ab63bbc788dfc24be3ecc79a279b9348c6a
Marius Karnauskas <marius.karnauskas@gmail.com>
2017-03-23 15:17:14 +0000

    tf-docs

---
+++ terraform-docs.rb
@@ -0,0 +1,10 @@
+class TerraformDocs < Formula
+    homepage "https://github.com/segmentio/terraform-docs/"
+    version '0.1.0'
+    url "https://github.com/segmentio/terraform-docs/releases/"\
+        "download/v#{version}/terraform-docs_darwin_amd64"
+
+    def install
+        bin.install "terraform-docs_darwin_amd64" => "terraform-docs"
+    end
+end
