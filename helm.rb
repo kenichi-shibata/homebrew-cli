0099bb93da9819732a688d4e6679b26d7eca6fe1
Kenichi Shibata <kenichi.shibata@condenastint.com>
2018-10-10 12:34:41 +0100

    Using 2.8.2 helm

--- helm.rb
+++ helm.rb
@@ -1,10 +1,11 @@
-class TerraformDocs < Formula
-    homepage "https://github.com/segmentio/terraform-docs/"
+class Helm < Formula
+    homepage "https://github.com/helm/helm/"
     version '2.8.2'
     url "https://storage.googleapis.com/kubernetes-helm/"\
         "helm-v#{version}-darwin-amd64.tar.gz"
 
     def install
-        bin.install "helm-v#{version}-darwin-amd64.tar.gz" => "helm"
+        bin.install 'helm'
+        #bin.install "helm-v#{version}-darwin-amd64.tar.gz" => "helm"
     end
 end
