9c9ab812df5b391dd0ce849e6413dc6ae27cb2bb
Kenichi Shibata <kenichi.shibata@condenastint.com>
2019-01-23 11:13:24 +0000

    Update eksctl.rb

--- eksctl.rb
+++ eksctl.rb
@@ -1,9 +1,9 @@
 class Eksctl < Formula
     desc 'a CLI for Amazon EKS'
     homepage 'https://eksctl.io'
-    version '0.1.0-alpha.6'
+    version '0.1.9'
     url "https://github.com/weaveworks/eksctl/releases/download/#{version}/eksctl_Darwin_amd64.tar.gz"
-    sha256 'a440bdd3c3e66b53fba63433350bebbc1208d0ed93753b2c7302247b247a8097'
+    sha256 'dd3757b4f30e031d45fb2d292b1b18497ad9868c9873d6fd98e047b859cae735'
 
     def install
         bin.install 'eksctl'
