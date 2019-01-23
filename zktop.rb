79190169ed229cc720dfe878aeb39b899ef3e7f9
Kenichi Shibata <kenichi.shibata@condenastint.com>
2019-01-23 09:26:03 +0000

    Delete zktop.rb

--- zktop.rb
+++
@@ -1,8 +0,0 @@
-class Zktop < Formula
-    homepage "https://github.com/phunt/zktop"
-    head "https://github.com/phunt/zktop.git", :using => :git
-
-    def install
-        bin.install "zktop.py"
-    end
-end
