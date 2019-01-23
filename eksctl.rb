3175fee01be41af322feb0aed7e93324fd1561c9
Kenichi Shibata <kenichi.shibata@bglgroup.co.uk>
2019-01-23 11:18:00 +0000

    Oops wrong version
    Signed-off-by: Kenichi Shibata <kenichi.shibata@bglgroup.co.uk>

--- eksctl.rb
+++ eksctl.rb
@@ -1,7 +1,7 @@
 class Eksctl < Formula
     desc 'a CLI for Amazon EKS'
     homepage 'https://eksctl.io'
-    version '0.1.9'
+    version '0.1.19'
     url "https://github.com/weaveworks/eksctl/releases/download/#{version}/eksctl_Darwin_amd64.tar.gz"
     sha256 'dd3757b4f30e031d45fb2d292b1b18497ad9868c9873d6fd98e047b859cae735'
 
