12e72c1340b2e758a781b16293b956fbf22f09e1
Kenichi Shibata <kenichi.shibata@bglgroup.co.uk>
2019-05-13 12:07:54 +0100

    Create chromium.rb

---
+++ chromium.rb
@@ -0,0 +1,20 @@
+# Linked https://github.com/Homebrew/homebrew-cask/blob/master/Casks/chromium.rb
+cask 'chromium' do
+  version '658918'
+  sha256 'eccd3fe8cce1378d607986f0c20c33a4cb02e15880c4263eb84616d24449fa18'
+
+  # commondatastorage.googleapis.com/chromium-browser-snapshots/Mac was verified as official when first introduced to the cask
+  url "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/#{version}/chrome-mac.zip"
+  appcast 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2FLAST_CHANGE?alt=media'
+  name 'Chromium'
+  homepage 'https://www.chromium.org/Home'
+
+  app 'chrome-mac/Chromium.app'
+
+  zap trash: [
+               '~/Library/Preferences/org.chromium.Chromium.plist',
+               '~/Library/Caches/Chromium',
+               '~/Library/Application Support/Chromium',
+               '~/Library/Saved Application State/org.chromium.Chromium.savedState',
+             ]
+end
